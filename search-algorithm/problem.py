216a0472fa263873f3e323785b53bb2614ddbb74
andre-grassi <andregrassi.j@gmail.com>
2025-09-02 01:31:26 -0300

    Refactor Problem class: simplify constructor and abstract methods

--- search-algorithm/problem.py
+++ search-algorithm/problem.py
@@ -1,21 +1,26 @@
+from abc import ABC, abstractmethod
+
+
 class Problem:
     """
     A class representing a problem in a search algorithm context.
-    It contains the initial state, goal state, and methods to check if a state is the goal.
     """
 
-    def __init__(self, initial_state, actions, transition_model, goal_test, path_cost):
+    def __init__(self, initial_state):
         self.initial_state = initial_state
-        self.actions = actions
-        self.transition_model = transition_model
-        self.goal_test = goal_test
-        self.path_cost = path_cost
 
-    def is_goal(self, state):
-        """
-        Check if the given state is the goal state.
+    @abstractmethod
+    def transition_model(state, action):
+        pass
 
-        :param state: The state to check.
-        :return: True if the state is the goal state, False otherwise.
-        """
-        return state == self.goal_state
+    @abstractmethod
+    def goal_test(state) -> bool:
+        pass
+
+    @abstractmethod
+    def actions(state) -> list:
+        pass
+
+    @abstractmethod
+    def action_cost(action) -> int | float:
+        pass
