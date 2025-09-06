2bace2f636b93aeb50a50f7b46d80fab21e3e075
Andre-Grassi <andregrassi.j@gmail.com>
2025-09-06 16:39:54 -0400

    Define class for problem

---
+++ search_algorithm/c++/data_structure/problem.cc
@@ -0,0 +1,24 @@
+#ifndef __PROBLEM_H__
+#define __PROBLEM_H__
+
+template <typename TState, typename TAction>
+class Problem {
+   public:
+    virtual ~Problem() = default;
+
+    virtual TState GetInitialState() const = 0;
+
+    // Goal test
+    virtual bool IsGoal(const TState& state) const = 0;
+
+    virtual std::vector<TAction> GetActions(const TState& state) const = 0;
+
+    // Transition model
+    virtual TState GetResult(const TState& state,
+                             const TAction& action) const = 0;
+
+    virtual double GetActionCost(const TState& state, const TAction& action,
+                                 const TState& new_state) const = 0;
+};
+
+#endif  // __PROBLEM_H__
