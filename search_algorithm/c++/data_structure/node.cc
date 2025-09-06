dfd6a4127794bd950ee87695bfa1b614595a8ac1
Andre-Grassi <andregrassi.j@gmail.com>
2025-09-06 16:19:27 -0400

    Add getters/setters for the Node

--- search_algorithm/c++/data_structure/node.cc
+++ search_algorithm/c++/data_structure/node.cc
@@ -1,3 +1,6 @@
+#ifndef __NODE_H__
+#define __NODE_H__
+
 #include <memory>
 
 template <typename TState, typename TAction>
@@ -9,11 +12,30 @@ class Node {
     float path_cost_;
 
    public:
-    explicit Node(StateType state,
-                  std::shared_ptr<Node<StateType, ActionType>> parent = nullptr,
-                  ActionType action = ActionType{}, double path_cost = 0.0)
+    explicit Node(TState state,
+                  std::shared_ptr<Node<TState, TAction>> parent = nullptr,
+                  TAction action = TAction{}, double path_cost = 0.0)
         : state_(std::move(state)),
           parent_(std::move(parent)),
           action_(action),
           path_cost_(path_cost) {}
+
+    const TState& get_state() const { return state_; }
+    void set_state(const TState& state) { state_ = state; }
+    void set_state(TState&& state) { state_ = std::move(state); }
+
+    std::shared_ptr<Node<TState, TAction>> get_parent() const {
+        return parent_;
+    }
+    void set_parent(std::shared_ptr<Node<TState, TAction>> parent) {
+        parent_ = std::move(parent);
+    }
+
+    const TAction& get_action() const { return action_; }
+    void set_action(const TAction& action) { action_ = action; }
+    void set_action(TAction&& action) { action_ = std::move(action); }
+
+    float get_path_cost() const { return path_cost_; }
+    void set_path_cost(float path_cost) { path_cost_ = path_cost; }
 };
+#endif  // __NODE_H__
