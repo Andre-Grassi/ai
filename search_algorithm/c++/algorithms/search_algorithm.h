89a4942170dcbdda6cf44afe8ee5578ce4e4d408
Andre-Grassi <andregrassi.j@gmail.com>
2025-09-09 03:10:35 -0400

    Add include guards and template implementation for BreadthFirstSearch

--- search_algorithm/c++/algorithms/search_algorithm.h
+++ search_algorithm/c++/algorithms/search_algorithm.h
@@ -1,3 +1,6 @@
+#ifndef SEARCH_ALGORITHM_BREADTH_FIRST_SEARCH_H
+#define SEARCH_ALGORITHM_BREADTH_FIRST_SEARCH_H
+
 #include "../data_structure/node.h"
 #include "../data_structure/problem.h"
 #include "../data_structure/problems/sliding_tile_problem.h"
@@ -6,3 +9,7 @@ namespace search_algorithm {
 template <typename State, typename Action>
 Node<State, Action>* BreadthFirstSearch(Problem<State, Action> const& problem);
 }
+
+// Include template implementation
+#include "breadth_first_search.tpp"
+#endif  // SEARCH_ALGORITHM_BREADTH_FIRST_SEARCH_H
