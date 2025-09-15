717403c9527e9e54836aa93727322e73fa790afb
andre-grassi <andregrassi.j@gmail.com>
2025-09-15 13:36:55 -0300

    Add comparison by Astar

--- search_algorithm/c++/data_structure/node_comparators.h
+++ search_algorithm/c++/data_structure/node_comparators.h
@@ -5,6 +5,7 @@
 #include <memory>
 
 #include "node.h"
+#include "problem.h"
 
 namespace node_comparators {
 // Min-heap (smallest cost first) based on path cost
@@ -13,6 +14,22 @@ struct CompareByPathCost {
     bool operator()(const std::shared_ptr<Node<TState, TAction>>& a,
                     const std::shared_ptr<Node<TState, TAction>>& b) const {
         return a->GetPathCost() > b->GetPathCost();
+    }
+};
+
+template <typename TState, typename TAction>
+struct CompareByAStar {
+   private:
+    // Problem reference to access heuristic function
+    Problem<TState, TAction> problem_;
+
+   public:
+    CompareByAStar(Problem<TState, TAction> problem) : problem_(problem) {}
+
+    bool operator()(const std::shared_ptr<Node<TState, TAction>>& a,
+                    const std::shared_ptr<Node<TState, TAction>>& b) const {
+        return (a->GetPathCost() + problem_.Heuristic(a->GetState())) >
+               (b->GetPathCost() + problem_.Heuristic(b->GetState()));
     }
 };
 }  // namespace node_comparators
