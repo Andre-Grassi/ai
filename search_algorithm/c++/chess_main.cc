bf505542885cfa5084e8bb6ad1632ae81893ba3e
Andre-Grassi <andregrassi.j@gmail.com>
2025-09-28 11:40:24 -0300

    Print actions that led to goal in chess main

--- search_algorithm/c++/chess_main.cc
+++ search_algorithm/c++/chess_main.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <stack>
 
 #include "algorithms/search_algorithm.h"
 #include "data_structure/node.h"
@@ -11,28 +12,48 @@ int main() {
     using Comparator = CompareByAStar<chess_board::State, chess_board::Action,
                                       chess_board::ChessCostType>;
 
-    // Create a Chess Board problem -- Parametro: 1 ou 2 pra definir o tabuleiro
-    // inicial igual no enunciado
-    auto problem = std::make_unique<chess_board::ChessBoardProblem>(2);
+    using ChessNode = Node<chess_board::State, chess_board::Action,
+                           chess_board::ChessCostType>;
+
+    // Create a Chess Board problem -- Parametro: 1 ou 2 pra definir o
+    // tabuleiro inicial igual no enunciado
+    auto problem = std::make_unique<chess_board::ChessBoardProblem>(1);
 
     // Get initial state
     auto initial_state = problem->GetInitialState();
-    std::cout << "Initial State Created Successfully!" << std::endl;
 
     // Display initial state
     problem->PrintState(initial_state);
 
-    std::shared_ptr<Node<chess_board::State, chess_board::Action,
-                         chess_board::ChessCostType>>
-        solution = search_algorithm::BestFirstSearch<
-            chess_board::State, chess_board::Action, chess_board::ChessCostType,
-            Comparator>(*problem);
+    std::shared_ptr<ChessNode> solution = search_algorithm::BestFirstSearch<
+        chess_board::State, chess_board::Action, chess_board::ChessCostType,
+        Comparator>(*problem);
 
-    if (solution) {
-        std::cout << "Goal State Found!" << std::endl;
-        problem->PrintState(solution->GetState());
-    } else
+    if (!solution) {
         std::cout << "Goal State Not Found." << std::endl;
+        return 1;
+    }
+
+    // Stack the actions from the solution (leaf) node to the root and then
+    // print them
+    std::stack<chess_board::Action> action_stack;
+
+    // Collect actions from leaf -> root, skipping root (it has no action)
+    for (std::shared_ptr<ChessNode> node = solution; node && node->GetParent();
+         node = node->GetParent())
+        action_stack.push(node->GetAction());
+
+    // Print Actions
+    while (!action_stack.empty()) {
+        chess_board::Action &top_action = action_stack.top();
+        action_stack.pop();
+
+        problem->PrintAction(top_action);
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
+    problem->PrintState(solution->GetState());
 
     return 0;
 }
