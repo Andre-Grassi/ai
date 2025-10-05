0ca3414cda03534a646088205db6e78f8c139f8c
Andre-Grassi <andregrassi.j@gmail.com>
2025-10-05 19:49:05 -0300

    Add tic tac toe example

---
+++ search_algorithm/c++/examples/tic_tac_toe_example.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include "algorithms/adversarial_search/adversarial_search_algorithm.h"
+#include "data_structure/adversarial_search/games/tic_tac_toe_game.h"
+
+int main() {
+    using namespace tic_tac_toe_game;
+
+    TicTacToeGame game;
+    game.PrintState(game.GetInitialState());
+
+    State current_state = game.GetInitialState();
+    while (!game.IsTerminal(current_state)) {
+        std::unique_ptr<Action> best_action =
+            adversarial_search_algorithm::MinimaxSearch(game, current_state);
+
+        std::unique_ptr<State> next_state =
+            game.GetResult(current_state, *best_action);
+        current_state = *next_state;
+
+        game.PrintState(current_state);
+    }
+
+    return 0;
+}
