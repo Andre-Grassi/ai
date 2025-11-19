132361d49741ef5f849952d2d7becfb295167fd5
Andre-Grassi <andregrassi.j@gmail.com>
2025-11-19 20:20:13 -0300

    Pass hash table to minimax

--- search_algorithm/c++/adugo_main.cc
+++ search_algorithm/c++/adugo_main.cc
@@ -9,13 +9,14 @@ int main() {
     AdugoGame game;
     game.PrintState(game.GetInitialState());
 
+    std::unordered_map<State, Utility> transposition_table;
     State current_state = game.GetInitialState();
     while (!game.IsTerminal(current_state)) {
         std::cout << std::endl;
 
         std::unique_ptr<Action> best_action =
-            adversarial_search_algorithm::HeuristicMinimaxSearch(game,
-                                                                 current_state);
+            adversarial_search_algorithm::HeuristicMinimaxSearch(
+                game, current_state, transposition_table);
 
         std::unique_ptr<State> next_state =
             game.GetResult(current_state, *best_action);
