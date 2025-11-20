1bf7049f413ae154d979213d3bd49fd8c6247de2
Andre-Grassi <andregrassi.j@gmail.com>
2025-11-20 12:02:25 -0300

    Add extern "C" linkage for C++ compatibility in tabuleiro.h

--- search_algorithm/c++/jogo_da_onca/server/tabuleiro.h
+++ search_algorithm/c++/jogo_da_onca/server/tabuleiro.h
@@ -1,8 +1,16 @@
 #ifndef __TABULEIRO_H__
 #define __TABULEIRO_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void tabuleiro_conecta(int, char**);
 void tabuleiro_envia(char*);
 void tabuleiro_recebe(char*);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
