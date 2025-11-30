74d25ca734495076fbdc0b2d6c38172a796ba0e7
Andre-Grassi <andregrassi.j@gmail.com>
2025-11-30 20:56:23 -0300

    Fix start_server jogo_da_onca dir

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,9 +2,9 @@
 # type `sudo visudo` and add the following line:
 # `your_username ALL=(ALL) NOPASSWD: /full/path/to/your/start_server.sh`
 
-redis-server ../server/redis.conf &
+redis-server ../jogo_da_onca/server/redis.conf &
 sleep 0.5
 
 # Start controller
-../server/controlador o 127.0.0.1 50 0
+../jogo_da_onca/server/controlador o 127.0.0.1 50 0
 sudo pkill -9 redis-server
